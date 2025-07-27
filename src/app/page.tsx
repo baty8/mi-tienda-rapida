cf6cc691490a0a344b0af385267281740c8db0b7
Bautista Aguerre <aguerrebau10@gmail.com>
2025-07-27 23:46:36 +0000

    bien mg ese icono ahora pon ese en ves del clip en el lgin el registro y

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -59,7 +59,10 @@ const VentaRapidaLogo = (props: React.SVGProps<SVGSVGElement>) => (
         strokeLinejoin="round" 
         {...props}
     >
-        <path d="M21.44 11.05l-9.19 9.19a6 6 0 0 1-8.49-8.49l9.19-9.19a4 4 0 0 1 5.66 5.66l-9.2 9.19a2 2 0 0 1-2.83-2.83l8.49-8.48" />
+        <path d="M6 2L3 6v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V6l-3-4z"/>
+        <line x1="3" x2="21" y1="6" y2="6"/>
+        <path d="M16 10a4 4 0 0 1-8 0"/>
+        <path d="m13 14-2 3-2-3"/>
     </svg>
 );
 
