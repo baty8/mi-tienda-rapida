3df8e620218fafb09fe654c18ae0f62cc29d1461
Bautista Aguerre <aguerrebau10@gmail.com>
2025-09-26 22:14:11 +0000

    no funciona los edpoints post path ya no creo que seal el codigo que pue

--- next.config.js
+++ next.config.js
@@ -17,6 +17,14 @@ const nextConfig = {
       },
     ],
   },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:9003/api/:path*', // Proxy to the new dev port
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
